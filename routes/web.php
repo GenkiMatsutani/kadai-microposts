d6e6e6febfc773589f5618b90a2e2f42342f4988
genki.matsutani <genki.matsutani@techacademy.jp>
2023-10-13 01:31:51 +0000

    follow, unfollow

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\UsersController; // 8で追記
-use App\Http\Controllers\MicropostsController; //9で追記 
+use App\Http\Controllers\UsersController; // 8その他のユーザー機能で追記
+use App\Http\Controllers\MicropostsController; //9投稿機能で追記 
+use App\Http\Controllers\UserFollowController;  // 11フォロー機能で追記
 
 /*
 |--------------------------------------------------------------------------
@@ -28,6 +29,13 @@ Route::get('/dashboard', [MicropostsController::class, 'index'])->middleware(['a
 require __DIR__.'/auth.php';
 
 Route::group(['middleware' => ['auth']], function () {                                    // 8追記
+    Route::group(['prefix' => 'users/{id}'], function () {                                          // 追記
+        Route::post('follow', [UserFollowController::class, 'store'])->name('user.follow');         // 追記
+        Route::delete('unfollow', [UserFollowController::class, 'destroy'])->name('user.unfollow'); // 追記
+        Route::get('followings', [UsersController::class, 'followings'])->name('users.followings'); // 追記
+        Route::get('followers', [UsersController::class, 'followers'])->name('users.followers');    // 追記
+    });                                                                                             // 追記
+    
     Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);     // 8追記
     Route::resource('microposts', MicropostsController::class, ['only' => ['store', 'destroy']]);   // 9で追記
 });        
